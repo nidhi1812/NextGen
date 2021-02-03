aeb26b86c940bd1ea77d39f63fdada3b4fc144e5
Nidhi Gupta <nidhi@Nidhis-MacBook-Air.local>
2021-02-03 16:28:41 -0500

    new Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-nextGen';
+  fruits : Array<any> = ['apple','banana','orange','cherry','kiwi'];
+  newFruits : Array< String> = [];
+  isChecked : Boolean =false;  
+
+  constructor(private fb: FormBuilder) { }
+
+  onChange(name:String , isChecked : Boolean) {
+    this.newFruits?.push(name);
+    const index = this.fruits.indexOf(name);
+if (index > -1) {
+  this.fruits.splice(index, 1);
+}
+    // const fruits = (this.form.controls. as FormArray);
+
+    // if (isChecked) {
+    //   fruits.push(new FormControl(name));
+    // } else {
+    //   const index = fruits.controls.findIndex(x => x.value === name);
+    //   fruits.removeAt(index);
+    // }
+  }
+  form = new FormGroup({
+    fruits: new FormControl(),
+  });
+  ngOnInit() {
+   
+  }
+
+  submit() {
+    console.log(this.form.value.name);
+  }
 }
